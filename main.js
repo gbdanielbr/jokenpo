0bd6a95b3ce62e29258522ff624b7fd000c3ea35
Daniel M <daniel.milli@hotmail.com>
2021-10-25 22:49:26 -0300

    prototype finished

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var playerOneChoise
-var playerTwoChoise
-var playerOneScore
-var playerTwoScore
+var playerOneChoice
+var playerTwoChoice
+var playerOneScore = 0
+var playerTwoScore = 0
 
 //1 == rock
 //2 == paper
@@ -10,46 +10,44 @@ var playerTwoScore
 //jogador 1 ganha: winner = 1
 //jogador 2 ganha: winner = 2
 
-function firstPlayerChoise(choise) {
-  playerOneChoise = choise
-  document
-    .getElementById('playerOne-choice-' + playerOneChoise)
-    .classList.add('selected')
+function firstPlayerChoice(choice) {
+  c = choice
+  document.getElementById('playerOne-choice-' + c).classList.add('selected')
 }
 
-function secondPlayerChoise(choise) {
-  playerTwoChoise = choise
+function secondPlayerChoice(choice) {
+  playerTwoChoice = choice
   document
-    .getElementById('playerTwo-choice-' + playerTwoChoise)
+    .getElementById('playerTwo-choice-' + playerTwoChoice)
     .classList.add('selected')
 }
 
 function play() {
   let winner
-  if (playerOneChoise == playerTwoChoise) {
+  if (c == playerTwoChoice) {
     winner = 0
     messages.textContent = `Empate`
-  } else if (playerOneChoise == 1 && playerTwoChoise == 2) {
+  } else if (c == 1 && playerTwoChoice == 2) {
     winner = 2
     playerTwoScore++
     messages.textContent = `O jogador ${winner} ganhou`
-  } else if (playerOneChoise == 1 && playerTwoChoise == 3) {
+  } else if (c == 1 && playerTwoChoice == 3) {
     winner = 1
     messages.textContent = `O jogador ${winner} ganhou`
     playerOneScore++
-  } else if (playerOneChoise == 2 && playerTwoChoise == 1) {
+  } else if (c == 2 && playerTwoChoice == 1) {
     winner = 1
     messages.textContent = `O jogador ${winner} ganhou`
     playerOneScore++
-  } else if (playerOneChoise == 2 && playerTwoChoise == 3) {
+  } else if (c == 2 && playerTwoChoice == 3) {
     winner = 2
     messages.textContent = `O jogador ${winner} ganhou`
     playerTwoScore++
-  } else if (playerOneChoise == 3 && playerTwoChoise == 1) {
+  } else if (c == 3 && playerTwoChoice == 1) {
     winner = 2
     messages.textContent = `O jogador ${winner} ganhou`
     playerTwoScore++
-  } else if (playerOneChoise == 3 && playerTwoChoise == 2) {
+  } else if (c == 3 && playerTwoChoice == 2) {
     winner = 1
     messages.textContent = `O jogador ${winner} ganhou`
     playerOneScore++
@@ -58,11 +56,23 @@ function play() {
   messages = document.getElementById('messages')
   messages.style.display = 'flex'
 
-  document
-    .getElementById('playerOne-choice-' + playerOneChoise)
-    .classList.remove('selected')
+  document.getElementById('playerOne-choice-' + c).classList.remove('selected')
 
   document
-    .getElementById('playerTwo-choice-' + playerTwoChoise)
+    .getElementById('playerTwo-choice-' + playerTwoChoice)
     .classList.remove('selected')
+
+  document.getElementById('playerOne-score').innerHTML = playerOneScore
+  document.getElementById('playerTwo-score').innerHTML = playerTwoScore
+}
+
+function reset() {
+  playerOneScore = 0
+  playerTwoScore = 0
+
+  document.getElementById('playerOne-score').innerHTML = playerOneScore
+  document.getElementById('playerTwo-score').innerHTML = playerTwoScore
+
+  messages = document.getElementById('messages')
+  messages.style.display = 'none'
 }
